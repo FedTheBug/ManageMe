cef13987bf319e88b637b98d03cf0dbfdbbe0206
FedTheBug <fahadmuntasir747@gmail.com>
2020-05-05 13:54:09 +0600

    User Interface and Controller Class Created

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 400));
         primaryStage.show();
 
     }

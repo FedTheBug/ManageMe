43012a7c318a18c84084367cf5cee277c90bf408
FedTheBug <fahadmuntasir747@gmail.com>
2020-05-07 01:00:32 +0600

    Several JFoenix Item Modified

--- src/sample/LogInController.java
+++ src/sample/LogInController.java
@@ -1,4 +1,37 @@
 package sample;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LogInController {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField LoginUsername;
+
+    @FXML
+    private PasswordField LoginPassword;
+
+    @FXML
+    private JFXButton LoginButton;
+
+    @FXML
+    private JFXButton SignUpButton;
+
+    @FXML
+    void initialize() {
+        SignUpButton.setOnAction(event -> {
+            System.out.println("SignUpButtonClicked");
+        });
+
+    }
 }
